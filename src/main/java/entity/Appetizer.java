6257e45ac0266b47bc44ca80bb83ba00d8f87d1e
Ivan Vlasenko <vlasenkoivan93@gmail.com>
2020-09-11 14:43:44 +0300

    change inheritance

--- src/main/java/entity/Appetizer.java
+++ src/main/java/entity/Appetizer.java
@@ -1,54 +1,9 @@
 package entity;
 
-import java.util.Objects;
 
-public class Appetizer {
-    private String name;
-    private String category;
-    private String cuisine;
+public class Appetizer extends Dish {
 
-    public Appetizer(String name, String category, String cuisine) {
-        this.name = name;
-        this.category = category;
-        this.cuisine = cuisine;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getCuisine() {
-        return cuisine;
-    }
-
-    public void setCuisine(String cuisine) {
-        this.cuisine = cuisine;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Appetizer that = (Appetizer) o;
-        return Objects.equals(name, that.name) &&
-                Objects.equals(category, that.category) &&
-                Objects.equals(cuisine, that.cuisine);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, category, cuisine);
+    public Appetizer(String name, String cuisine, String category) {
+        super(name, cuisine, category);
     }
 }
--- src/main/java/entity/Dish.java
+++ src/main/java/entity/Dish.java
@@ -3,13 +3,11 @@ package entity;
 import java.util.Objects;
 
 public class Dish {
-    private int id;
     private String name;
     private String cuisine;
     private String category;
 
-    public Dish(int id, String name, String cuisine, String category) {
-        this.id = id;
+    public Dish(String name, String cuisine, String category) {
         this.name = name;
         this.cuisine = cuisine;
         this.category = category;
@@ -31,14 +29,6 @@ public class Dish {
         this.category = category;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
@@ -52,19 +42,13 @@ public class Dish {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Dish dish = (Dish) o;
-        return id == dish.id &&
-                Objects.equals(name, dish.name) &&
+        return Objects.equals(name, dish.name) &&
                 Objects.equals(cuisine, dish.cuisine) &&
                 Objects.equals(category, dish.category);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, cuisine, category);
-    }
-
-    @Override
-    public String toString() {
-        return name.toUpperCase() + cuisine + category;
+        return Objects.hash(name, cuisine, category);
     }
 }
